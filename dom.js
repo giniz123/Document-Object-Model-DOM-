ab686a164754b42574a8615dc9e650df3d29c7f3
giniz123 <giniz.osti@gmail.com>
2025-11-21 23:21:48 +1100

    HTML manipulation by using DOM

--- dom.js
+++ dom.js
@@ -17,7 +17,23 @@
 // document.writeln("End of console");
 // document.writeln("End of console");
 
-const elm = document.querySelector(".message");
-const val = elm.innerText;
-elm.innerHTML = "<h3>Server messages have been updated</h3>";
-console.log(elm);
+// const elm = document.querySelector(".message");
+// const val = elm.innerText;
+// elm.innerHTML = "<h3>Server messages have been updated</h3>";
+// console.log(elm);
+
+const elm = document.querySelector("body");
+
+// const textNode = document.createTextNode("Hello this is text node");
+// elm.appendChild(textNode);
+const newElement = document.createElement("p");
+newElement.innerText =
+  "Hello, this is paragraph element added from Javascript!";
+
+// elm.appendChild(newElement);
+
+// elm.insertBefore(newElement, document.querySelector(".display"));
+
+// elm.removeChild(document.querySelector(`[href="google.com"]`));
+
+elm.replaceChild(newElement, document.querySelector(`[href="google.com"]`));
